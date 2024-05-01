82a17735dcdaf1a0fc6548fb1f20aea7813c54ef
DC-CC-21 <handstands1000@gmail.com>
2024-05-01 15:02:48 -0400

    updated puzzle fetching functions

--- src/app/lib/puzzleDefs.tsx
+++ src/app/lib/puzzleDefs.tsx
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const puzzlePath = "public/puzzles";
+const puzzlePath = "./public/puzzles";
 
 export function getAllDirectories() {
     return fs
@@ -9,6 +9,11 @@ export function getAllDirectories() {
         .map((dirent: any) => dirent.name);
 }
 export function importAllOfType(type: string): string[] {
+    console.log("importAllOfType:", `${puzzlePath}/${type}`)
+    console.log("dirname:", __dirname)
+    console.log("public", fs.readdirSync("./public"))
+    console.log("puzzlePath", fs.readdirSync(puzzlePath))
+
     return fs.readdirSync(`${puzzlePath}/${type}`).filter((file: string) => {
         const fileExtension = path.extname(file).toLowerCase();
         return (
