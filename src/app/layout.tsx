742efcc2bfb3bbb3acf732a897cc7b90f6cdd16c
DC-CC-21 <handstands1000@gmail.com>
2024-05-03 22:15:35 -0400

    updated sw

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,27 +23,6 @@ export const metadata: Metadata = {
         capable: true,
         statusBarStyle: "default",
         title: APP_DEFAULT_TITLE,
-        // startUpImage: [],
-    },
-    formatDetection: {
-        telephone: false,
-    },
-    openGraph: {
-        type: "website",
-        siteName: APP_NAME,
-        title: {
-            default: APP_DEFAULT_TITLE,
-            template: APP_TITLE_TEMPLATE,
-        },
-        description: APP_DESCRIPTION,
-    },
-    twitter: {
-        card: "summary",
-        title: {
-            default: APP_DEFAULT_TITLE,
-            template: APP_TITLE_TEMPLATE,
-        },
-        description: APP_DESCRIPTION,
     },
 };
 
--- src/app/sw.ts
+++ src/app/sw.ts
@@ -20,16 +20,6 @@ const serwist = new Serwist({
     clientsClaim: true,
     navigationPreload: true,
     runtimeCaching: defaultCache,
-    fallbacks: {
-        entries: [
-            {
-                url: "/offline",
-                matcher({ request }) {
-                    return request.destination === "document";
-                },
-            },
-        ],
-    },
     importScripts: ["custom-sw.js"],
 });
 
