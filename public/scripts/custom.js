c45840b419b0df1617654f714d5af45fcf2a1dc4
DC-CC-21 <handstands1000@gmail.com>
2024-06-14 15:23:51 -0400

    working on fixing url problem

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -7,16 +7,30 @@ setCanvasQuality(canvas, window.innerWidth, window.innerWidth);
 setCanvasSize(canvas, canvas.width * 0.6, canvas.height * 0.6);
 
 document.getElementById("customFile").addEventListener("change", (event) => {
-    const file = event.target.files[0];
-    const url = URL.createObjectURL(file);
+    // const file = event.target.files[0];
+    // const url = URL.createObjectURL(file);
+    // const img = new Image();
+    // img.src = url;
+    // img.onload = () => {
+    //     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    //     const data = canvas.toDataURL("image/jpeg", 1);
+    //     localStorage.setItem("customPuzzle", data);
+    // };
+    readImage(event.target);
+});
+function readImage(input) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#00ffff";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    let imgSrc = "";
+    if (input.value !== "") {
+        imgSrc = window.URL.createObjectURL(input.files[0]);
+    }
     const img = new Image();
-    img.src = url;
-    img.onload = () => {
-        ctx.fillStyle = "#00ffff";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+    img.onload = function () {
         ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-
         const data = canvas.toDataURL("image/jpeg", 1);
         localStorage.setItem("customPuzzle", data);
     };
-});
+    img.src = imgSrc;
+}
