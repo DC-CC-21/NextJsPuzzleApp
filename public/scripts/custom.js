b6c59dbfee400d76f6442c7df37d27fe240bdeb4
DC-CC-21 <handstands1000@gmail.com>
2024-06-14 15:14:21 -0400

    fixed object url

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -8,20 +8,15 @@ setCanvasSize(canvas, canvas.width * 0.6, canvas.height * 0.6);
 
 document.getElementById("customFile").addEventListener("change", (event) => {
     const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => {
+    const url = URL.createObjectURL(file);
+    const img = new Image();
+    img.src = url;
+    img.onload = () => {
         ctx.fillStyle = "#00ffff";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-        const img = new Image();
-        img.src = reader.result;
-        img.onload = () => {
-            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
 
-            const data = canvas.toDataURL("image/jpeg", 1);
-            localStorage.setItem("customPuzzle", data)
-
-        };
+        const data = canvas.toDataURL("image/jpeg", 1);
+        localStorage.setItem("customPuzzle", data);
     };
 });
-
