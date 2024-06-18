b256dfedcf249e1d5d2ad4586a4d2aa96caf7dbe
DC-CC-21 <handstands1000@gmail.com>
2024-06-18 13:17:15 -0400

    added new puzzles

--- public/scripts/custom.js
+++ public/scripts/custom.js
@@ -5,19 +5,21 @@ let quality = 1;
 
 setCanvasQuality(canvas, window.innerWidth, window.innerWidth);
 setCanvasSize(canvas, canvas.width * 0.6, canvas.height * 0.6);
+ctx.fillStyle = "#00ffff";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-document.getElementById("customFile").addEventListener("change", (event) => {
-    // const file = event.target.files[0];
-    // const url = URL.createObjectURL(file);
-    // const img = new Image();
-    // img.src = url;
-    // img.onload = () => {
-    //     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-    //     const data = canvas.toDataURL("image/jpeg", 1);
-    //     localStorage.setItem("customPuzzle", data);
-    // };
-    readImage(event.target);
-});
+// document.getElementById("customFile").addEventListener("change", (event) => {
+//     // const file = event.target.files[0];
+//     // const url = URL.createObjectURL(file);
+//     // const img = new Image();
+//     // img.src = url;
+//     // img.onload = () => {
+//     //     ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+//     //     const data = canvas.toDataURL("image/jpeg", 1);
+//     //     localStorage.setItem("customPuzzle", data);
+//     // };
+//     readImage(event.target);
+// });
 function readImage(input) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#00ffff";
